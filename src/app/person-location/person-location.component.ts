d38c8793bf68322b90a053baf6a04bac1bc0e81c
edgewardo <edgewardo@gmail.com>
2024-02-04 16:55:25 -0700

    initial

--- src/app/person-location/person-location.component.ts
+++ src/app/person-location/person-location.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Person } from '../person';
 
 @Component({
   selector: 'app-person-location',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      person-location works!
-    </p>
+  <section class="listing">
+    <h2 class="listing-heading">{{ myperson.firstName }} {{ myperson.lastName}}</h2>
+    <p class="listing-location">Height: {{ myperson.height }}</p>
+    <p class="listing-location">Weight: {{ myperson.weight }}</p>
+  </section>
   `,
   styleUrl: './person-location.component.css'
 })
 export class PersonLocationComponent {
-
+  @Input() myperson!: Person;
 }
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,19 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PersonLocationComponent } from '../person-location/person-location.component';
+import { Person } from '../person';
 
 @Component({
   selector: 'app-person',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    PersonLocationComponent
+  ],
   template: `
   <section>
     <form>
-      <input type="text" placeholder="Filter by name">
+      <input type="text" placeholder="Filter by city">
       <button class="primary" type="button">Search</button>
     </form>
+  </section>
+  <section class="results">
+    <app-person-location *ngFor="let myperson of personList" [myperson]="myperson"></app-person-location>
   </section>
   `,
   styleUrl: './person.component.css'
 })
 export class PersonComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  personList: Person[] = [
+    {
+      id: 1,
+      firstName: 'Eric',
+      lastName: 'Ward',
+      height: 68,
+      weight: 200,
+    },
+    {
+      id: 2,
+      firstName: 'Sandy',
+      lastName: 'Ward',
+      height: 64,
+      weight: 190,
+    },
+
+  ];
 }
